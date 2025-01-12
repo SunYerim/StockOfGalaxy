99e262ed12822da830c25afe002b2dd35fa7decd
SunYerim <yaelim6@pukyong.ac.kr>
2025-01-12 12:56:50 +0900

    refactor: #1. DTO 네이밍 형식 camel 변경

--- backend/stock/src/main/java/com/sog/stock/domain/dto/websocket/StockPriceResponseDTO.java
+++ backend/stock/src/main/java/com/sog/stock/domain/dto/websocket/StockPriceResponseDTO.java
@@ -6,10 +6,10 @@ import lombok.Data;
 public class StockPriceResponseDTO {
     // 현재가, 전일대비, 전일대비율, 전일대비부호
 
-    private String stock_code; // 종목번호 - 0
-    private String stock_prpr; // 현재가 - 2
-    private String prdy_vrss_sign; // 전일 대비 부호 - 3
-    private String prdy_vrss; // 전일대비 - 4
-    private String prdy_ctrt; // 전일대비율 - 5
+    private String stockCode; // 종목번호 - 0
+    private String stockPrpr; // 현재가 - 2
+    private String prdyVrssSign; // 전일 대비 부호 - 3
+    private String prdyVrss; // 전일대비 - 4
+    private String prdyCtrt; // 전일대비율 - 5
 
 }
